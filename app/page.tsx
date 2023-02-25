d59959aaf0e18f95c415262c30134e393d680851
Daniel Van Cuylenburg <email@danielvanc.com>
2023-02-25 14:47:43 +0000

    chore: switched to nextjs

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="p-10 text-center">
+      <h1 className="text-2xl">Welcome</h1>
+    </div>
+  );
+}
