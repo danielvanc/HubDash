2db33f2be283571bdd9f0aaa0d69b6a6aaa8289a
Daniel Van Cuylenburg <email@danielvanc.com>
2023-07-01 16:22:03 +0100

    feat: removed auth checks

--- app/(index)/layout.tsx
+++ app/(index)/layout.tsx
@@ -1,8 +1,4 @@
 import "server-only";
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import CONFIG from "lib/config.json";
 import "@/app/tailwind.css";
 
 export default async function RootLayout({
@@ -10,14 +6,6 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const supabase = createServerComponentClient({ cookies });
-
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
-
-  if (session) return redirect(CONFIG.LOGGED_IN);
-
   return (
     <html lang="en">
       <body>
